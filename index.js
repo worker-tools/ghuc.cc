9e27ee4a84058c30a8b27d678b748ec304c3ac4c
Florian Klampfer <mail@qwtel.com>
2022-04-10 20:09:10 +0700

    Add styles

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import { ok, badRequest, temporaryRedirect } from '@worker-tools/response-creato
 import { WorkerRouter } from '@worker-tools/router';
 import { provides, createMiddleware, combine } from '@worker-tools/middleware';
 import { StorageArea } from '@worker-tools/cloudflare-kv-storage';
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-import { html, HTMLResponse, HTMLContent } from '@worker-tools/html';
+import { html, HTMLResponse } from '@worker-tools/html';
 
 const navigator = self.navigator || { userAgent: 'Cloudflare Workers' }
 
@@ -17,13 +18,14 @@ const layout = (title, content) => html`<html>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>${title}</title>
+  <link rel="stylesheet" href="/_public/index.css">
 </head>
 
 <body>
   <main style="width:800px;margin:auto">
     <h1>GHUC.CC</h1>
     <span>ghuc.cc = GitHub User Content Carbon Copy<br>
-      Your handy redirection service for <strong>Deno</strong> to import TypeScript <em>directly from GitHub</em>.</span>
+      Your friendly neighborhood redirection service for <strong>Deno 🦕</strong> to import code <em>directly from GitHub</em>.</span>
     <br />
     <br />
     ${content}
@@ -39,9 +41,13 @@ const layout = (title, content) => html`<html>
 
 export const mkPage = ({ user, repo, branchOrTag, path }, url) => {
   return new HTMLResponse(layout('ghuc.cc', html`<div>
-  <!-- <a href="${url}">${new URL(new URL(url).pathname, 'https://ghuc.cc')}</a><br> -->
+  <!-- <code>import * as ${user.replaceAll('-', '_') + '__' + repo.replaceAll('-', '_')} from '<a href="${url}">${new URL(new URL(url).pathname, 'https://ghuc.cc')}</a>'</code><br><br> -->
+  <!-- <code>import {} from '<a href="${url}">${new URL(new URL(url).pathname, 'https://ghuc.cc')}</a>'</code><br><br> -->
   <div style="display:flex; justify-content:space-between">
-    Points to: /${user}/${repo}/${branchOrTag}/${path}
+    <div>
+      📦 ${new URL(new URL(url).pathname, 'https://ghuc.cc')}<br/>
+      ➡️ <span class="muted">https://raw.githubusercontent.com/${user}/${repo}/${branchOrTag}/${path}</span>
+    </div>
     <div>
       <a href="https://raw.githubusercontent.com/${user}/${repo}/${branchOrTag}/${path}">Raw</a>
       |
@@ -61,7 +67,7 @@ export const mkPage = ({ user, repo, branchOrTag, path }, url) => {
 </div>`))
 }
 
-export const mkInfo = () => {
+export const mkInfo = (response) => {
   return new HTMLResponse(layout('ghuc.cc',
     html`<div>
       Needs to match pattern <code>/:user/:repo{\@:version}?/:path(.*)</code>. Examples:
@@ -71,7 +77,7 @@ export const mkInfo = () => {
         <li><a href="/qwtel/typed-array-utils@0.2.2/index.ts">${new URL('/qwtel/typed-array-utils@0.2.2/index.ts', 'https://ghuc.cc')}</a></li>
       </ul>
 
-    </div>`))
+    </div>`), response)
 }
 
 export const mkError = (response, error) => {
@@ -108,20 +114,44 @@ const getBranchOrTag = async ({ user, repo, version }, { waitUntil }) => {
   }
 }
 
-const router = new WorkerRouter(provides(['text/html', '*/*']))
-  .get('/:user/:repo(@?[^@]+){@:version([^/]+)}?/:path(.*)', async (req, { match, type, waitUntil }) => {
-    const { pathname: { groups: { user, repo, version, path } } } = match;
+const handlePrefix = prefix => request => {
+  // compute the default (e.g. / -> index.html)
+  let defaultAssetKey = mapRequestToAsset(request)
+  let url = new URL(defaultAssetKey.url)
+
+  // strip the prefix from the path for lookup
+  url.pathname = url.pathname.replace(prefix, '/')
+
+  // inherit all other props from the default request
+  return new Request(url, defaultAssetKey)
+}
+
+const assetsRouter = new WorkerRouter()
+  .get('*', (req, ctx) => getAssetFromKV(ctx.event, { mapRequestToAsset: handlePrefix('/_public') }))
+  .recover('*', provides(['text/html', '*/*']), (req, { type, error, response }) => {
+    if (type === 'text/html') return mkError(response, error)
+    return response;
+  })
+
+const router = new WorkerRouter(provides(['text/html', '*/*']), { debug: self.DEBUG })
+  .use('/_public/*', assetsRouter)
+  .get('/:handle/:repo(@?[^@]+){@:version([^/]+)}?/:path(.*)', async (req, { match, type, waitUntil }) => {
+    const { pathname: { groups: { handle, repo, version, path } } } = match;
     // console.log({ user, repo, version, path })
 
-    const usr = user.startsWith('@') ? user.substring(1) : user
-    const branchOrTag = await getBranchOrTag({ user: usr, repo, version }, { waitUntil })
+    const user = handle.startsWith('@') ? handle.substring(1) : handle
+    const branchOrTag = await getBranchOrTag({ user, repo, version }, { waitUntil })
 
-    if (type === 'text/html') return mkPage({ user: usr, repo, branchOrTag, path }, req.url)
-    return temporaryRedirect(`https://raw.githubusercontent.com/${usr}/${repo}/${branchOrTag}/${path}`);
+    if (type === 'text/html') return mkPage({ user, repo, branchOrTag, path }, req.url)
+    return temporaryRedirect(`https://raw.githubusercontent.com/${user}/${repo}/${branchOrTag}/${path}`);
   })
   .get('/favicon.ico', () => ok()) // TODO
-  .any('*', (req, { type }) => {
+  .get('/', (req, { type }) => {
     if (type === 'text/html') return mkInfo()
+    return ok("Needs to match pattern '/:user/:repo{\@:version}?/:path(.*)'")
+  })
+  .any('*', (req, { type }) => {
+    if (type === 'text/html') return mkInfo(badRequest())
     return badRequest("Needs to match pattern '/:user/:repo{\@:version}?/:path(.*)'")
   })
   .recover('*', provides(['text/html', '*/*']), (req, { type, error, response }) => {
@@ -130,4 +160,3 @@ const router = new WorkerRouter(provides(['text/html', '*/*']))
   })
 
 self.addEventListener('fetch', router);
-
